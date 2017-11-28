2812634758f305e892b2bb662eb10e1fb9dbdbe5
Guest User <Guest@epicodus-25.local>
2017-11-28 13:48:30 -0800

    add user relation to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,48 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :find_question, only:[:show, :edit, :update, :destroy]
   def index
     @questions = Question.all
   end
 
   def show
-    @questions = Question.all
   end
 
   def new
-    @question = Question.new
+    @question = current_user.questions.build
   end
 
   def create
-    @question = Question.create(question_params)
-
+    @question = current_user.questions.build(question_params)
     if @question.save
       redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+    redirect_to question_path(@question)
     else
       render :edit
     end
   end
 
+  def destroy
+    @question.destroy
+    redirect_to root_path
+  end
+
   private
   def question_params
     params.require(:question).permit(:title, :description)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
